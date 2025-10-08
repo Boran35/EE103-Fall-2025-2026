3bdb4368b1937dd78d37a3b5893a19ac72b048df
Boran35 <boranaydin@std.iyte.edu.tr>
2025-10-08 23:12:18 +0300

    EE103

---
+++ Lecture 2/codes/test.py
@@ -0,0 +1,17 @@
+b = ":" 
+c = ")"
+s1 = b + 2*c
+print (s1)
+
+f = "a"
+g = " b"
+h = "3"
+s2 = (f+g)*int(h)
+print (s2)
+
+s = "abc"
+len(s)
+chars = len(s)
+print(chars)
+
+import pdb;pdb.set_trace()
