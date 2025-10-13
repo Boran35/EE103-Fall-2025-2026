2aafd48f7df045ffb9496b36a49a19afd8aae47a
Boran <boranaydin@std.iyte.edu.tr>
2025-10-13 13:25:01 +0300

    EE103

--- Lecture 2/codes/test.py
+++ Lecture 2/codes/test.py
@@ -1,17 +1,41 @@
-b = ":" 
-c = ")"
-s1 = b + 2*c
-print (s1)
+#b = ":" 
+#c = ")"
+#s1 = b + 2*c
+#print (s1)
 
-f = "a"
-g = " b"
-h = "3"
-s2 = (f+g)*int(h)
-print (s2)
+#f = "a"
+#g = " b"
+#h = "3"
+#s2 = (f+g)*int(h)
+#print (s2)
 
-s = "abc"
-len(s)
-chars = len(s)
-print(chars)
+#s = "abc"
+#len(s)
+#chars = len(s)
+#print(chars)
 
-import pdb;pdb.set_trace()
+#a = "the"
+#b = 3
+#c = "musketeers"
+#print(a, b, c)
+#print(a + str(b) + c)
+
+#text = input("Type anything: ")
+#print(3*text)
+
+#num1 = input("Type a number: ")
+#print(2*num1)
+
+#num2 = int(input("Type a number:"))
+#print(4*num2)
+
+#verb = input("Type a verb: ")
+#print("I can",verb, "better than you")
+#print(5*(verb+" "))
+
+x = int(input("What x to find the cube root of? "))
+g = int(input("What guess to start with?"))
+
+print("Current estimated cubed = ", g**3)
+next_g = g - ((g**3 - x)/(3*g**2))
+print("Next guess to try = ", next_g)
