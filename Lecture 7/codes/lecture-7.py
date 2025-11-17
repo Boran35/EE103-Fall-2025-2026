2a4864d04fc0de77f863fad6e5e7e83f8a73f8bd
Boran <boranaydin@std.iyte.edu.tr>
2025-11-17 15:04:56 +0300

    EE103

--- Lecture 7/codes/lecture-7.py
+++ Lecture 7/codes/lecture-7.py
@@ -1,10 +1,11 @@
 def div_by(n, d):
     """ n and d are ints > 0
-        Return True if d divides n evenly and False otherwise """
-    return n % d == 0
-def test_div_by(): 
-    assert div_by(6, 3) == True
-    assert div_by(7, 3) == False
-    assert div_by(10, 5) == True
-    assert div_by(10, 4) == False
-    assert div_by(1, 1) == True
+        Return True if d divides n evenly and False otherwise
+    """
+    if n%d == 0:
+        return True
+    else:
+        return False
+    
+print(div_by(10, 2))  # Should return True
+print(div_by(10, 3))  # Should return False
